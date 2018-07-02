67a295134d71186d4b882a1d1d095fff91b2807f
HokNan_Loue <34534604+190434957@users.noreply.github.com>
2018-07-03 02:03:24 +0800

    service

--- src/main/java/team/a9043/privacy_assessment_system/controller/TestController.java
+++ src/main/java/team/a9043/privacy_assessment_system/controller/TestController.java
@@ -3,17 +3,23 @@ package team.a9043.privacy_assessment_system.controller;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.a9043.privacy_assessment_system.service.TestService;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 @RestController
 public class TestController {
     private final TestService testService;
-    private Future<String> getJSONTask;
+    private Map<String, Future<String>> TaskMap;
 
     @Autowired
     public TestController(TestService testService) {
@@ -22,6 +28,7 @@ public class TestController {
 
     @GetMapping("/analysis")
     public String analysis() throws IOException, InterruptedException {
+        String id = UUID.randomUUID().toString();
         String cmdStr_linux = "python3 /home/weibo/Spider/all_jsondata.py 1645171780";
         File file = new File("/home/weibo/Spider/" + 1645171780);
         file.delete();
@@ -30,27 +37,27 @@ public class TestController {
         InputStream stream = proc.getInputStream();
 
         testService.getJsonResErr(errStream);
-        getJSONTask = testService.getJsonRes(stream);
+        TaskMap.put(id, testService.getJsonRes(stream));
 
-        return "{\"success\": true}";
+        return "{\"success\": true, \"id\": \"" + id + "\"}";
     }
 
     @GetMapping("/getResult")
-    public String getResult() throws ExecutionException, InterruptedException, IOException {
+    public String getResult(@RequestParam("id") String id) throws ExecutionException, InterruptedException, IOException {
         JSONObject jsonObject = new JSONObject();
-        if (getJSONTask == null) {
+        if (TaskMap.get(id) == null) {
             jsonObject.put("err", true);
             jsonObject.put("success", false);
             jsonObject.put("done", true);
             return jsonObject.toString();
         }
-        if (!getJSONTask.isDone()) {
+        if (!TaskMap.get(id).isDone()) {
             jsonObject.put("process", 10);
             char[] chars = new char[3];
             File file = new File("/home/weibo/Spider/" + 1645171780);
             if (file.exists()) {
                 FileReader fileReader = new FileReader(file);
-                if(fileReader.read(chars) != -1) {
+                if (fileReader.read(chars) != -1) {
                     jsonObject.put("process", Integer.valueOf(String.valueOf(chars).trim()));
                 }
             }
@@ -59,7 +66,7 @@ public class TestController {
             jsonObject.put("done", false);
             return jsonObject.toString();
         }
-        jsonObject = new JSONObject(getJSONTask.get());
+        jsonObject = new JSONObject(TaskMap.get(id).get());
         jsonObject.put("success", true);
         jsonObject.put("err", false);
         jsonObject.put("done", true);
